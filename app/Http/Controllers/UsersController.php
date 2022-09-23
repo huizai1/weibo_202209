2e14c93c268d7d19063bc0c534b8dcec806fecf1
huizai1 <237002893@qq.com>
2022-09-23 09:03:51 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    // 创建用户的页面
     public function create()
     {
         return view('users.create');
+    }
+
+    // 显示用户个人信息的页面
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
